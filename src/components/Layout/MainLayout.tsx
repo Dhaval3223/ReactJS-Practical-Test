e539837310807045aa41913ae7fe5d1d7de9b7ce
vitalintel <pp@vitalintel.tech>
2025-06-29 21:15:58 +0530

    feat: added theming

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,26 +1,31 @@
-import { Box, Toolbar } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
+import { useState } from 'react';
 
 export default function MainLayout() {
+  const [mini, setMini] = useState(false);
+  const handleToggleSidebar = () => setMini(m => !m);
+  const theme = useTheme();
   return (
-    <Box sx={{ display: "flex" }}>
-      <Navbar />
-      <Sidebar />
+    <Box sx={{ display: "flex", height: "100vh" }}>
+      <Sidebar mini={mini} />
       <Box
         component="main"
         sx={{
           flexGrow: 1,
           minHeight: "100vh",
           height: "100vh",
+          overflow: 'auto',
           display: "flex",
           flexDirection: "column",
           p: 0,
-          width: 'calc(100vw - 280px)',
+          width: mini ? 'calc(100vw - 64px)' : 'calc(100vw - 220px)',
+          bgcolor: theme.palette.mode === 'dark' ? '#1B2431' : '#F5F6FA',
         }}
       >
-        <Toolbar />
+        <Navbar onToggleSidebar={handleToggleSidebar} />
         <Outlet />
       </Box>
     </Box>
